18ec14bc085a2d41bd561f31f7bed4fa9f9a4ba0
mmahalwy <mohamed@peek.com>
2015-09-15 17:58:35 -0700

    Optional child

--- src/CopyToClipboard.js
+++ src/CopyToClipboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import copy from 'copy-to-clipboard';
 
-
 const onClick = (text, onCopy) => () => {
   copy(text);
   if (onCopy) {
@@ -9,18 +8,21 @@ const onClick = (text, onCopy) => () => {
   }
 };
 
-
 const CopyToClipboard = React.createClass({
   propTypes: {
     text: React.PropTypes.string.isRequired,
-    onCopy: React.PropTypes.func
+    onCopy: React.PropTypes.func,
+    children: React.PropTypes.array
   },
 
 
   render() {
-    const {text, onCopy, ...props} = this.props;
+    const {text, onCopy, children, ...props} = this.props;
+    const elem = React.Children.only(children);
 
-    return <button onClick={onClick(text, onCopy)} {...props} />;
+    return React.cloneElement(elem, Object.assign({
+      onClick: onClick(text, onCopy)
+    }, props));
   }
 });
 
