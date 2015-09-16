32c335d873d2052013c66b0715f0d17144b7a10b
Nik Butenko <nik@butenko.me>
2015-09-16 20:55:55 +1000

    Windows compatibility - build, start

--- webpack.config.bower.js
+++ webpack.config.bower.js
@@ -2,28 +2,38 @@
 
 
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 
 module.exports = {
   devtool: 'source-map',
-  entry: './src/example/Example.js',
-  output: {filename: 'bundle.js', path: path.resolve('example')},
+  entry: './src/index.js',
+  output: {
+    filename: require('./package.json').name + '.js',
+    path: path.resolve('build'),
+    library: 'Component',
+    libraryTarget: 'umd'
+  },
   plugins: [
-    new HtmlWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
     })
   ],
-
   module: {
     loaders: [
       {test: /\.js$/, loader: 'babel', include: [path.resolve('src')]}
     ]
   },
   resolve: {extensions: ['', '.js']},
-  stats: {colors: true}
+  stats: {colors: true},
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  }
 };
