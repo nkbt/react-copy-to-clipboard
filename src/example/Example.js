10e9229a8477c96006fd963b5194ea8eba20c4c9
Daniel Friesen <daniel@nadir-seen-fire.com>
2015-10-11 01:33:23 -0700

    Cleanup and update for React 0.14
    - Exclusively depend on ^0.14.0 of react
    - Use react-dom's render in example
    - In example don't render directly into document.body, this causes a React warning

--- src/example/Example.js
+++ src/example/Example.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
+const appRoot = document.createElement('div');
 
-React.render(<App />, document.body);
+appRoot.id = 'app';
+document.body.appendChild(appRoot);
+ReactDOM.render(<App />, appRoot);
