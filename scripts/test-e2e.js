2022eec85457d2f32d41f8e66513879de270c44f
Nik Butenko <nik@butenko.me>
2015-10-24 21:57:20 +1100

    Create test-e2e script to consequently run: - install selenium - selenium server - dev server - nightwatch tests

---
+++ scripts/test-e2e.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+var selenium = require('selenium-standalone');
+var cp = require('child_process');
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require(path.resolve(__dirname, '..', 'webpack.config.js'));
+var logDir = process.env.LOG_DIR ?
+  path.resolve(process.env.LOG_DIR) : path.resolve(__dirname, '..', 'reports');
+
+var seleniumLog = fs.createWriteStream(path.resolve(logDir, 'selenium.log'));
+var nightwatchRunner = path
+  .resolve(__dirname, '..', 'node_modules', 'nightwatch', 'bin', 'runner.js');
+
+
+function startServer(cb) {
+  var server = new WebpackDevServer(webpack(webpackConfig), {quiet: true});
+
+  server.listen(8080, 'localhost', cb);
+}
+
+
+function onServerStarted(seleniumChild) {
+  return function (err) {
+    if (err) {
+      console.error(err);
+      seleniumChild.kill('SIGHUP');
+      return process.exit(1);
+    }
+
+    cp.fork(nightwatchRunner, ['--config', path.resolve(__dirname, '..', 'nightwatch.json')])
+      .on('error', function (error) {
+        console.error(error);
+        seleniumChild.kill('SIGHUP');
+        return process.exit(1);
+      })
+      .on('close', function (code) {
+        seleniumChild.kill('SIGHUP');
+        process.exit(code);
+      });
+  };
+}
+
+
+function onSeleniumStarted(err, seleniumChild) {
+  if (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+  seleniumChild.stdout.pipe(seleniumLog);
+  seleniumChild.stderr.pipe(seleniumLog);
+  startServer(onServerStarted(seleniumChild));
+}
+
+
+function onSeleniumInstalled(err) {
+  if (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+
+  selenium.start({debug: true}, onSeleniumStarted);
+}
+
+
+selenium.install({}, onSeleniumInstalled);
