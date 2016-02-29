0f2a8c25eff1bc1005981fadc2125dc86c649bd6
Nik Butenko <nik@butenko.me>
2016-02-29 23:45:12 +1100

    Fix code for new ESLint rules

--- src/Component.js
+++ src/Component.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 
-const Component = React.createClass({
-  render() {
-    return <div>Component</div>;
-  }
-});
+const Component = () => <div>Component</div>;
 
 
 export default Component;
