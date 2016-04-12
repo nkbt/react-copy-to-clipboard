d1b9da30b85cff19ef57ac692a8755da36afc41f
Nik Butenko <nik@butenko.me>
2016-04-13 00:29:15 +1000

    Remove leftovers

--- test/ReactComponentTemplate-test.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape';
-import ReactComponentTemplate from '../src/ReactComponentTemplate';
-
-
-test('ReactComponentTemplate', t => {
-  t.ok(ReactComponentTemplate instanceof Function, 'should be function');
-  t.end();
-});
