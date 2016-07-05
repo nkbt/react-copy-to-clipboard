18cbbee1d51c633fb656d768a3a106486df8c360
Nik Butenko <nik@butenko.me>
2016-07-05 19:16:12 +1000

    Bypass onClick if passed from outside as prop without override

--- src/CopyToClipboard.js
+++ src/CopyToClipboard.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import copy from 'copy-to-clipboard';
 
-const onClick = (text, onCopy) => () => {
-  copy(text);
-  if (onCopy) {
-    onCopy(text);
-  }
-};
 
 const CopyToClipboard = React.createClass({
   propTypes: {
     text: React.PropTypes.string.isRequired,
     children: React.PropTypes.element.isRequired,
+    onClick: React.PropTypes.func,
     onCopy: React.PropTypes.func
   },
 
 
+  onClick(e) {
+    const {text, onCopy, onClick} = this.props;
+
+    copy(text);
+    if (onCopy) {
+      onCopy(text);
+    }
+
+    // Bypass onClick if it was present
+    if (onClick) {
+      onClick(e);
+    }
+  },
+
+
   render() {
-    const {text, onCopy, children, ...props} = this.props;
+    const {
+      text: _text,
+      onCopy: _onCopy,
+      onClick: _onClick,
+      children,
+      ...props
+    } = this.props;
     const elem = React.Children.only(children);
 
-    return React.cloneElement(elem, {
-      ...props,
-      onClick: onClick(text, onCopy)
-    });
+    return React.cloneElement(elem, {...props, onClick: this.onClick});
   }
 });
 
